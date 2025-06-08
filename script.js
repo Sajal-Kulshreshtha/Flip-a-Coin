608454345e2e4311c1efef1538cc6b85972c32b8
Sajal-Kulshreshtha <sajalkulshreshtha9@gmail.com>
2025-06-09 01:45:18 +0530

    whole src
    done whole src

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const btn =document.getElementById("btn")
+const text=document.getElementById("result")
+const img=document.getElementById("img")
+function flip(){
+     return Math.random()<0.5;
+}
+
+btn.addEventListener("click",()=>{
     
+    img.classList.add("flipped")
+    setTimeout(() => {
+    const result = flip()
+
+    let string=""
+    if (result===true){
+    img.src="heads.png"
+    text.innerText="Heads";
+    
+}
+    else{
+        img.src="tails.png"
+        text.innerText="Tails"
+}
+img.classList.remove("flipped")
+
+img.classList.remove("flipped")
+  }, 600)
+})
